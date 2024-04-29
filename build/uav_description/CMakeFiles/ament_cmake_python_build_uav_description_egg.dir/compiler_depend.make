776f2c04e0a76fd775e1714b194983563635338f
kayyyak <61357514+thitiporn-su@users.noreply.github.com>
2024-04-29 17:20:39 +0700

    update

--- build/uav_description/CMakeFiles/ament_cmake_python_build_uav_description_egg.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for ament_cmake_python_build_uav_description_egg.
-# This may be replaced when dependencies are built.
--- build/uav_description/ament_cmake_python/uav_description/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='uav_description',
-    version='0.0.0',
-    packages=find_packages(
-        include=('uav_description', 'uav_description.*')),
-)
